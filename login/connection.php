9117f3e803c97f32e272d9126774beb49a5364b3
jerry <jerry.jge@gmail.com>
2022-11-10 11:41:43 +0800

    Update connection.php

--- login/connection.php
+++ login/connection.php
@@ -3,7 +3,7 @@
 
 $dbhost = "localhost";
 $dbuser = "root";           #default user
-$dbpass = "lsyxgjrHZHGe556!";
+$dbpass = "your pass here";
 $dbname = "accounts_db";
 
 if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname, 3307))
